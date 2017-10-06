2f2474beadb8e26f079065bdf682d4c11d07fa55
Timothy Cevallos <timcev@gmail.com>
2017-10-06 17:47:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,13 +7,13 @@ class Driver{
     store.drivers.push(this)
   }
   trips(){
-    return store.trips.filter(function(trip){
-        return trip.driverId == this.id
+    return store.trips.filter(function(trips){
+        return trips.driverId == this.id
       })
   }
   passengers(){
-    return this.trips().map(function(trip){
-      return trip.passenger()
+    return this.trips().map(function(trips){
+      return trips.passenger()
     })
   }
 }
