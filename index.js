58c954e8ee36f594680c9678bacee157712ace65
Timothy Cevallos <timcev@gmail.com>
2017-10-06 17:32:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -36,4 +36,10 @@ class Trip {
     this.id = ++tripId
     store.trips.push(this)
   }
+  driver(){
+   return store.drivers.find((driver) => { return driver.id === this.driverId })
+ }
+ passenger(){
+   return store.passengers.find((passenger) => { return passenger.id === this.passengerId })
+ }
 }
