ba698561bf3e3cb8cfe36fe3c9a316fba3acf894
Timothy Cevallos <timcev@gmail.com>
2017-10-06 18:00:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -26,12 +26,12 @@ class Passenger{
     store.passengers.push(this)
   }
   trips(){
-     return store.trips.filter(function(trip) {
+     return store.trips.filter((trip)=> {
        return trip.passengerId == this.id
      })
    }
    drivers(){
-     return this.trips().map(function(trip) {
+     return this.trips().map((trip)=> {
        return trip.driver()
      })
    }
@@ -46,13 +46,9 @@ class Trip {
     store.trips.push(this)
     }
     driver(){
-      return store.drivers.find(function(driver){
-        return driver.id === this.driverId
-      })
-    }
-    passenger(){
-      return store.passenger.find(function(passenger){
-        return passenger.id === this.passengerId
-      })
-    }
+     return store.drivers.find((driver) => { return driver.id === this.driverId })
+   }
+   passenger(){
+     return store.passengers.find((passenger) => { return passenger.id === this.passengerId })
+   }
 }
