4e8cb72a8b2c9f02f844c230dfbc58cd59c5a5cf
Timothy Cevallos <timcev@gmail.com>
2017-10-06 17:21:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ class Driver{
   constructor(name){
     this.name = name
     this.id = ++driverId
+     store.drivers.push(this)
   }
   trips(){
     return store.trips.filter(function(el, i , array){
